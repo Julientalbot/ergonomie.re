225357807d1e5e73fe358a86b2daefb2f0a26eb6
Julientalbot <julien.talbot@ergonomie.re>
2024-01-20 19:51:42 +0400

    blog

--- app/blog/_assets/categoriesAndAuthors/authors.js
+++ app/blog/_assets/categoriesAndAuthors/authors.js
@@ -54,16 +54,16 @@ const { authorSlugs } = slugs
 const authors = [
   {
     // The slug to use in the URL, from the authorSlugs object above.
-    slug: authorSlugs.marc,
+    slug: authorSlugs.julien,
     // The name to display in the author's bio. Up to 60 characters.
-    name: "Marc Lou",
+    name: "Julien Talbot",
     // The job to display in the author's bio. Up to 60 characters.
-    job: "Maker of ByeDispute",
+    job: "Dirigeant d'ergonomie.re",
     // The description of the author to display in the author's bio. Up to 160 characters.
     description:
-      "Marc is a developer and an entrepreneur. He's built 20 startups in the last 3 years. 6 were profitable and 3 were acquired. He's currently building ByeDispute, the #1 Stripe Chargebacks Protection tool.",
+      "Je suis Julien Talbot, consultant en ergonomie et fondateur d'ergonomie.re",
     // The avatar of the author to display in the author's bio and avatar badge. It's better to use a local image, but you can also use an external image (https://...)
-    avatar: "/blog/images/marc.png",
+    avatar: "/blog/images/julien.png",
     // A list of social links to display in the author's bio.
     socials: [
       {
