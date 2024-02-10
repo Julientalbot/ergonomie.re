b2f16fb22a83eaa94c6a986cebcdc3d7e3cc2edf
julien <julien.talbot@ergonomie.re>
2024-02-10 12:51:00 +0400

    updated Hero

--- app/page.js
+++ app/page.js
@@ -1,3 +1,4 @@
+import { Suspense } from 'react'
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 import Problem from "@/components/Problem";
@@ -10,19 +11,21 @@ import CTA from "@/components/CTA";
 import FAQ from "@/components/FAQ";
 import Footer from "@/components/Footer";
 
-// Structure : Sensibilisation / (reco sociale : logo d'entreprises) / Reconnaissance du problème / Présentation de la solution / Avantages spécifiques / CTA / A propos avec reconnaissance a-sociale comme les diplômes 
+// Structure : Sensibilisation / (reco sociale : logo d'entreprises) / Reconnaissance du problème / Présentation de la solution / Avantages spécifiques (pourquoi nous ?) / CTA / A propos avec reconnaissance a-sociale comme les diplômes 
 
 export default function Home() {
   return (
     <>
-      <Header /> {/*  */}
+    <Suspense>
+      <Header />
+      </Suspense> {/* logo / [votre problème / notre solution / ce qui nous différencie / qui je suis] / CTA(s) */}
       <main>
-        <Hero /> {/*  */}
-        <Problem /> {/* Agitation du problème = listing précis des effets de l'absence d'ergonomie. */}
-        <FeaturesAccordion2 /> {/* Présentation de la solution et de ses bénéfices. Equivalent de : "vous utilisez telle méthode, c'est l'ancienne, voilà avec ma méthode ce qu'on pourrait avoir."*/}
-        <FeaturesListicle /> 
+        <Hero /> {/* unaware / votre problème : le manque d'ergonomie coûte cher */}
+        <Problem /> {/* problem aware : Agitation du problème = listing précis des effets de l'absence d'ergonomie et d'EFC */}
+        <FeaturesAccordion2 /> {/* solution aware : Présentation de la solution et de ses bénéfices. Equivalent de : "vous utilisez telle méthode, c'est l'ancienne, voilà avec ma méthode ce qu'on pourrait avoir."*/}
+        <FeaturesListicle /> {/* product aware : (1) un ergonome vraiment ergonome (je ne vends pas de chaise), (2)  */}
         <Testimonials3 /> {/*  */}
-        <CalEmbed /> {/*  */}
+        <CalEmbed /> {/* most aware : */}
         <About /> {/*  */}
         <CTA /> {/*  */}
         <FAQ /> {/*  */}
