4538a81349abdc6b305185894e710f72e9a292f6
Romain P <piveteauit@gmail.com>
2024-08-07 18:53:32 +0300

    ok

--- app/dashboard/layout.js
+++ app/dashboard/layout.js
@@ -2,6 +2,8 @@ import { redirect } from "next/navigation";
 import config from "@/config";
 import { cookies } from "next/headers";
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 // This is a server-side component to ensure the user is logged in.
 // If not, it will redirect to the login page.
@@ -19,5 +21,9 @@ export default async function LayoutPrivate({ children }) {
   //   redirect(config.auth.loginUrl);
   // }
 
-  return <>{children}</>;
+  return <>
+    <Header />
+      {children}
+    <Footer />
+  </>;
 }
