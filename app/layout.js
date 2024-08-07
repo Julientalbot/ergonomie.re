9d4dfcd0d74bf2c6622abbdf45ed194eb8ec10bd
Romain P <piveteauit@gmail.com>
2024-08-07 16:58:21 +0300

    support pwa

--- app/layout.js
+++ app/layout.js
@@ -18,7 +18,10 @@ export const viewport = {
 
 // This adds default SEO tags to all pages in our app.
 // You can override them in each page passing params to getSOTags() function.
-export const metadata = getSEOTags();
+export const metadata = {
+  ...getSEOTags(),
+  manifest: "/manifest.json",
+};
 
 export default function RootLayout({ children }) {
   return (
--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const withPWA = require("@ducanh2912/next-pwa").default({
   aggressiveFrontEndNavCaching: true,
   reloadOnOnline: true,
   swcMinify: true,
-  disable: false , //process.env.NODE_ENV === "development",
+  disable: process.env.NODE_ENV === "development",
   workboxOptions: {
     disableDevLogs: process.env.NODE_ENV !== "development",
   },
